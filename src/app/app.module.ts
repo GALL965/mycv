47059470c90384d4ea3f01a477fb0da5d0067d4f
GALL965 <bonaisdecamote45@gmail.com>
2025-04-05 23:05:48 -0600

    mycv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { WorkExperienceComponent } from './work-experience/work-experience.component';
-import { EducationComponent } from './education/education.component';
-import { SkillsComponent } from './skills/skills.component';
 import { CertificatesComponent } from './certificates/certificates.component';
 import { LanguagesComponent } from './languages/languages.component';
 import { InterestsComponent } from './interests/interests.component';
 
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HeaderComponent } from './header/header.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    WorkExperienceComponent,
-    EducationComponent,
-    SkillsComponent,
-    CertificatesComponent,
-    LanguagesComponent,
-    InterestsComponent
+    declarations: [
+  AppComponent,
+  HeaderComponent,
+  WorkExperienceComponent,
+  EducationComponent,
+  SkillsComponent,
+  CertificatesComponent,
+  LanguagesComponent,
+  InterestsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDvB_5lqLQJP_3OMACJ-FL4BBCSqkLV5Oc",
+    authDomain: "my-cv-e292f.firebaseapp.com",
+    projectId: "my-cv-e292f",
+    storageBucket: "my-cv-e292f.appspot.com",
+    messagingSenderId: "18704728562",
+    appId: "1:18704728562:web:a61c4eba2ffc95a2deeb6b"
+  }
+};
+
